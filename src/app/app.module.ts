6ae7e369437496985ad342c513c2715db43a59e6
Dhinesh <dhineshravi95@outlook.com>
2020-10-11 12:54:45 +0530

    Login and Signup components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './Auth/signup/signup.component';
+import { LoginComponent } from './Auth/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
