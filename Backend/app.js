fdeef5169c4118b73829b44b0cf0deb22446e4d5
Dhinesh <dhineshravi95@outlook.com>
2020-10-11 20:50:23 +0530

    Basic Backend and DB Structure

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,19 @@
 var express = require('express');
 
+var mongoose = require('mongoose');
 var bodyParser= require('body-parser');
 
 var app=express();
+
+mongoose.connect('mongodb://localhost:27017/fullstackPractice',{useNewUrlParser:true})
+.then(() => {
+  console.log('Connection Successful')
+})
+.catch(() => {
+  console.log('Connection Failed');
+})
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.listen(3000);
